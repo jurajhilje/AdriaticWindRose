204e00e58f85b70017f1fe34f7f1787f1385b2c8
Juraj Hilje <jhilje@gmail.com>
2016-01-04 22:18:06 +0100

    Add locationManager property and conform ViewController to CLLocationManagerDelegate

--- AdriaticWindRose/ViewController.swift
+++ AdriaticWindRose/ViewController.swift
@@ -1,7 +1,24 @@
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager:CLLocationManager!
+    
+    // MARK: - View lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.startUpdatingHeading()
     }
+    
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManager(manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        print(newHeading.magneticHeading)
+    }
+    
 }
