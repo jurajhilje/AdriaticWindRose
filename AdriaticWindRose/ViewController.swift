c48110897b0856cb62fd70cc89f1d12b0b152aae
Juraj Hilje <jhilje@gmail.com>
2016-01-04 22:58:39 +0100

    Implement updateImageAngle method

--- AdriaticWindRose/ViewController.swift
+++ AdriaticWindRose/ViewController.swift
@@ -1,8 +1,15 @@
 import UIKit
 import CoreLocation
 
+extension Double {
+    var degreesToRadians: CGFloat {
+        return CGFloat(self) * CGFloat(M_PI) / 180.0
+    }
+}
+
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    @IBOutlet weak var image: UIImageView!
     var locationManager:CLLocationManager!
     
     // MARK: - View lifecycle
@@ -15,10 +22,24 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingHeading()
     }
     
+    // MARK: - Image
+    
+    func updateImageAngle(degree degree: Double) {
+        UIView.animateWithDuration(
+            0.25,
+            delay: 0,
+            options: UIViewAnimationOptions.CurveEaseOut,
+            animations: {
+                self.image.transform = CGAffineTransformMakeRotation(-degree.degreesToRadians)
+            },
+            completion: { finish in }
+        )
+    }
+    
     // MARK: - CLLocationManagerDelegate
     
     func locationManager(manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
-        print(newHeading.magneticHeading)
+        self.updateImageAngle(degree: Double(newHeading.magneticHeading))
     }
     
 }
